05f42e1e2ffdef8a2b0e55811569cefc82c18aeb
Anirudh patki <83394182+idiotboxai@users.noreply.github.com>
2023-03-12 08:39:46 +0530

    Update paper_download.py

--- paper_download.py
+++ paper_download.py
@@ -2,8 +2,9 @@ import requests
 import os
 import time
 import webbrowser
+from tqdm import tqdm
 def open_telegram():
-    webbrowser.open("https://t.me/msbtehelp")
+  webbrowser.open("https://t.me/msbtehelp")
 print("\033]8;;https://t.me/msbtehelp\a" + "Join telegram" + "\033]8;;\a")
 print("\033[2m" + """Created by - idiotboxai""" + "\033[0m")
 print("\033[38;5;208m" + """
@@ -29,54 +30,78 @@ print("\033[38;5;208m" + """
                                            
 """ + "\033[0m")
 time.sleep(3)
-# Other code goes here
-# Prompt user for subject code and year
-subject_code = input("Enter subject code: ").upper()
-print("Choose year from the following:")
-print("QPS22 - Summer 2022")
-print("QPW19 - Winter 2019")
-print("QPS19 - Summer 2019")
-print("QPW18 - Winter 2018")
-print("QPS18 - Summer 2018")
-print("QPW17 - Winter 2017")
-print("QPS17 - Summer 2017")
+def download_paper(paper_type):
+    subject_code = input("What is the subject code? ")
+    print("Choose year from the following:")
+    print("QPS22 - Summer 2022 enter 1")
+    print("QPW19 - Winter 2019 enter 2")
+    print("QPS19 - Summer 2019 enter 3")
+    print("QPW18 - Winter 2018 enter 4")
+    print("QPS18 - Summer 2018 enter 5")
+    print("QPW17 - Winter 2017 enter 6")
+    print("QPS17 - Summer 2017 enter 7")
+    year = input("Enter the year code: ")
 
-year = input("Enter year (e.g. QPW19): ").upper()
-
-year_dict = {
-    'QPS22': 'Summer 2022',
-    'QPW19': 'Winter 2019',
-    'QPS19': 'Summer 2019',
-    'QPW18': 'Winter 2018',
-    'QPS18': 'Summer 2018',
-    'QPW17': 'Winter 2017',
-    'QPS17': 'Summer 2017'
-}
-
-if year not in year_dict.keys():
-    print("Invalid year.")
-else:
-
-    paper_code = year[-2:]
-    url = f"https://msbte.org.in/portal/msbte_files/questionpaper_search/{year}/{subject_code}.pdf"
-
-    # Check if URL exists and download PDF if it does
-    if requests.get(url).status_code == 200:
-        # Create directory if it doesn't exist
-        directory = f"{subject_code}_{year_dict[year]}"
-        if not os.path.exists(directory):
-            os.makedirs(directory)
-
-        # Check if file already exists
-        file_path = os.path.join(directory, f"{subject_code}_{paper_code}.pdf")
-        if os.path.exists(file_path):
-            print(f"{file_path} already exists.")
-        else:
-            # Download PDF
-            response = requests.get(url)
-            with open(file_path, 'wb') as f:
-                f.write(response.content)
-            print(f"Successfully downloaded {subject_code}_{paper_code}.pdf")
+    # convert year code to format used in URL
+    if year == "1":
+        year_code = "QPS22"
+    elif year == "2":
+        year_code = "QPW19"
+    elif year == "3":
+        year_code = "QPS19"
+    elif year == "4":
+        year_code = "QPW18"
+    elif year == "5":
+        year_code = "QPS18"
+    elif year == "6":
+        year_code = "QPW17"
+    elif year == "7":
+        year_code = "QPS17"
     else:
-        print("Paper not found.")
+        print("Invalid year code entered")
+        return
 
+    # construct the URL based on the paper type and user input
+    if paper_type == "question":
+        url = f"https://msbte.org.in/portal/msbte_files/questionpaper_search/{year_code}/{subject_code}.pdf"
+        file_name = f"{subject_code}_question_paper.pdf"
+    elif paper_type == "answer":
+        url = f"https://msbte.org.in/portal/msbte_files/ModalAns/{year_code}/{subject_code}.pdf"
+        file_name = f"{subject_code}_answer_paper.pdf"
+    else:
+        print("Invalid paper type entered")
+        return
+
+    # create directory if it does not exist
+    if not os.path.exists(subject_code):
+        os.mkdir(subject_code)
+
+    # check if a directory with subject code already exists
+    else:
+        # if it does, create another directory with year code
+        os.mkdir(os.path.join(subject_code, year_code))
+        subject_code = os.path.join(subject_code, year_code)
+
+    # download the file and save it to the directory
+    response = requests.get(url, stream=True)
+    file_size = int(response.headers.get('content-length', 0))
+    block_size = 1024
+    progress_bar = tqdm(total=file_size, unit='iB', unit_scale=True)
+    with open(os.path.join(subject_code, file_name), 'wb') as f:
+        for data in response.iter_content(block_size):
+            progress_bar.update(len(data))
+            f.write(data)
+    progress_bar.close()
+
+    print("Download complete!")
+
+# prompt user for paper type
+paper_type = input("Which paper do you want to download? Enter 1 for question paper and 2 for answer paper: ")
+
+# call download_paper function based on user input
+if paper_type == "1":
+    download_paper("question")
+elif paper_type == "2":
+    download_paper("answer")
+else:
+    print("Invalid input, please enter 1 or 2")
